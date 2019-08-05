e5ec793f4e61244474f83c47d0790ee4b2bf5ea1
Joao Schincariol <joao.schincariol@hotmail.com>
2019-08-05 15:51:46 +0200

    first commit

---
+++ src/main/java/com/carcharging/carcharging/CarchargingApplication.java
@@ -0,0 +1,13 @@
+package com.carcharging.carcharging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarchargingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarchargingApplication.class, args);
+	}
+
+}
